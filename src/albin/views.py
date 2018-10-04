b64f72dd9443191cda36c06902fc3b54bbcd8170
Thomas Niebler <thomasniebler@users.noreply.github.com>
2018-10-04 15:58:06 +0200

    removd raw path

--- src/albin/views.py
+++ src/albin/views.py
@@ -6,6 +6,6 @@ import json
 
 @albin.route("/s/<keyword>")
 def list_comics_with_keyword_in_title(keyword):
-    xkcd = pandas.read_pickle("/home/iisy.lan/thomas.niebler/xkcd_metadata.pkl")
+    xkcd = pandas.read_pickle("xkcd_metadata.pkl")
     relevant = xkcd[xkcd["safe_title"].str.lower().str.contains(keyword.lower())]
     return jsonify(json.loads(relevant.to_json(orient="records")))
